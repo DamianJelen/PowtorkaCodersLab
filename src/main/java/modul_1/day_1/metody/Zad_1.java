8676e168a85690a012422551877e246ad418cfb3
Damian Jeleń <damian161@o2.pl>
2022-08-29 19:38:36 +0200

    methods ex 1

---
+++ src/main/java/modul_1/day_1/metody/Zad_1.java
@@ -0,0 +1,11 @@
+package modul_1.day_1.metody;
+
+public class Zad_1 {
+    public static void main(String[] args) {
+        System.out.println("3x5 = " + multiply(3,5));
+    }
+
+    protected static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+}

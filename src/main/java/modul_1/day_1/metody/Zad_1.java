afd70e5401c8ef8407b392a1220793831d64e1cf
Damian Jeleń <damian161@o2.pl>
2022-08-29 19:46:44 +0200

    methods ex 2

--- src/main/java/modul_1/day_1/metody/Zad_1.java
+++ src/main/java/modul_1/day_1/metody/Zad_1.java
@@ -3,9 +3,14 @@ package modul_1.day_1.metody;
 public class Zad_1 {
     public static void main(String[] args) {
         System.out.println("3x5 = " + multiply(3,5));
+        System.out.println("5^2 = " + square(5));
     }
 
     protected static int multiply(int num1, int num2) {
         return num1 * num2;
+    }
+
+    protected static int square(int num) {
+        return (int) Math.pow(num, 2d);
     }
 }

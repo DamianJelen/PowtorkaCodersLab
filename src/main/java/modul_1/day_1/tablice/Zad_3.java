48ce962e87277eb5341a210419a3020b147580db
Jeleń Damian <Damian.Jelen@mfipr.gov.pl>
2022-08-31 11:38:51 +0200

    arrays ex 3

---
+++ src/main/java/modul_1/day_1/tablice/Zad_3.java
@@ -0,0 +1,11 @@
+package modul_1.day_1.tablice;
+
+import java.util.Arrays;
+
+public class Zad_3 {
+    public static void main(String[] args) {
+        int[] intArr = new int[10];
+        Arrays.fill(intArr, 2);
+        System.out.println(Arrays.toString(intArr));
+    }
+}

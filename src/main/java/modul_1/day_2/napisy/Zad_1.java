b79ce04032b46b0c24764805b9fcad6f08e5d6be
Jeleń Damian <Damian.Jelen@mfipr.gov.pl>
2022-09-01 12:04:53 +0200

    strings ex 1

---
+++ src/main/java/modul_1/day_2/napisy/Zad_1.java
@@ -0,0 +1,8 @@
+package modul_1.day_2.napisy;
+
+public class Zad_1 {
+    public static void main(String[] args) {
+        String txt = "Ala ma kota";
+        System.out.println(txt.charAt(txt.length()-2));
+    }
+}

83d427b560d16e10459a5e696a1c833686a81e51
Jeleń Damian <Damian.Jelen@mfipr.gov.pl>
2022-09-08 12:29:22 +0200

    files ex 6 add method checlFileExists

--- src/main/java/modul_1/day_4/files/Zad_6.java
+++ src/main/java/modul_1/day_4/files/Zad_6.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Zad_6 {
     public static void main(String[] args) {
         System.out.println(countChars("day_4/files/Zad_5/text5.txt"));
         System.out.println(countWords("day_4/files/Zad_5/text5.txt"));
+        System.out.println(checkFileExists());
     }
 
     public static int countChars(String fileName) {
@@ -40,5 +42,11 @@ public class Zad_6 {
         }
 
         return result;
+    }
+
+    public static boolean checkFileExists() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Wpisz nazwę pliku/ścieżkę z plikiem i sprawdź czy istnieje: ");
+        return Files.exists(Paths.get(scanner.nextLine()));
     }
 }

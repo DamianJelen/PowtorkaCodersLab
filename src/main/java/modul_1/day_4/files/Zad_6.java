d5107db66aa84d8c4e5935e630959d2b0ffe45af
Jeleń Damian <Damian.Jelen@mfipr.gov.pl>
2022-09-08 14:10:37 +0200

    files ex 6 add method revrite

--- src/main/java/modul_1/day_4/files/Zad_6.java
+++ src/main/java/modul_1/day_4/files/Zad_6.java
@@ -1,9 +1,11 @@
 package modul_1.day_4.files;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Scanner;
 
 public class Zad_6 {
@@ -11,6 +13,7 @@ public class Zad_6 {
         System.out.println(countChars("day_4/files/Zad_5/text5.txt"));
         System.out.println(countWords("day_4/files/Zad_5/text5.txt"));
         System.out.println(checkFileExists());
+        rewrite();
     }
 
     public static int countChars(String fileName) {
@@ -48,5 +51,25 @@ public class Zad_6 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Wpisz nazwę pliku/ścieżkę z plikiem i sprawdź czy istnieje: ");
         return Files.exists(Paths.get(scanner.nextLine()));
+    }
+
+    public static void rewrite() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Wpisz nazwę pliku/ścieżkę z plikiem np. day_4/files/Zad_5/text5.txt: ");
+        Path path = Paths.get(scanner.nextLine());
+        if(Files.exists(path)) {
+            String resultFileName = "day_4/files/Zad_6/" + String.valueOf(path.getFileName()).replaceAll(".txt", "_2.txt");
+            try {
+                Files.copy(path, Paths.get(resultFileName), StandardCopyOption.REPLACE_EXISTING);
+                FileWriter fileWriter = new FileWriter(resultFileName, true);
+                for (String line : Files.readAllLines(path)) {
+                    fileWriter.append("\n").append(line);
+                }
+                fileWriter.close();
+            } catch (IOException e) {
+                System.out.println("Problem z plikiem");
+                e.printStackTrace();
+            }
+        }
     }
 }

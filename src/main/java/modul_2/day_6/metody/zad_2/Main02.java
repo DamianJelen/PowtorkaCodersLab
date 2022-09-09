db2c040dc307fa742227d8f89d7896359e6649be
damian161@o2.pl <damian161@o2.pl>
2022-09-09 19:44:28 +0200

    day 6 methods ex 2

---
+++ src/main/java/modul_2/day_6/metody/zad_2/Main02.java
@@ -0,0 +1,10 @@
+package modul_2.day_6.metody.zad_2;
+
+public class Main02 {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("Daniel");
+        person.setAge(39);
+        
+    }
+}

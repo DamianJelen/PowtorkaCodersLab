68a8f1fd8e50e8d66526441b28ca55b770aee961
damian161@o2.pl <damian161@o2.pl>
2022-09-09 20:46:33 +0200

    day 6 constructor ex 1

---
+++ src/main/java/modul_2/day_6/konstruktor/zad_1/Burger.java
@@ -0,0 +1,14 @@
+package modul_2.day_6.konstruktor.zad_1;
+
+public class Burger {
+    private String size;
+    private double price;
+
+    public Burger() {
+    }
+
+    public Burger(String size, double price) {
+        this.size = size;
+        this.price = price;
+    }
+}
---
+++ src/main/java/modul_2/day_6/konstruktor/zad_1/MainBurger.java
@@ -0,0 +1,8 @@
+package modul_2.day_6.konstruktor.zad_1;
+
+public class MainBurger {
+    public static void main(String[] args) {
+        Burger burger = new Burger();
+        Burger burger_2 = new Burger("XL", 22.5);
+    }
+}

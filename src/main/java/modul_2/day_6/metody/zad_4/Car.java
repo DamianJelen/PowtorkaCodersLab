c2e047ac382afb5356bd853c8220c5c2a6999e4d
damian161@o2.pl <damian161@o2.pl>
2022-09-09 19:53:32 +0200

    day 6 methods ex 4

---
+++ src/main/java/modul_2/day_6/metody/zad_4/Car.java
@@ -0,0 +1,40 @@
+package modul_2.day_6.metody.zad_4;
+
+public class Car {
+    private String brand;
+    private String model;
+    private double price;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}

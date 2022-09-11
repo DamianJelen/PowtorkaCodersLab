0bb513dbaf218d662b0f8bd27f185398e3993c93
damian161@o2.pl <damian161@o2.pl>
2022-09-11 15:41:18 +0200

    day 6 add zad_2 object Author

---
+++ src/main/java/modul_2/day_6/dodatkowe/zad_2/Author.java
@@ -0,0 +1,15 @@
+package modul_2.day_6.dodatkowe.zad_2;
+
+public class Author {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String pseudonym;
+
+    public Author(int id, String firstName, String lastName, String pseudonym) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.pseudonym = pseudonym;
+    }
+}

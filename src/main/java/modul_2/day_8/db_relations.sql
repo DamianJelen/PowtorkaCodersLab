8f6edd6d5a1f2cf6be738850a5fe9eccabf95591
damian161@o2.pl <damian161@o2.pl>
2022-09-15 12:07:05 +0200

    day 8 db relation many-to-many

--- src/main/java/modul_2/day_8/db_relations.sql
+++ src/main/java/modul_2/day_8/db_relations.sql
@@ -110,3 +110,101 @@ CREATE TABLE categories_sub (
                                 PRIMARY KEY(id),
                                 FOREIGN KEY(main_id) REFERENCES categories(id)
 );
+
+------------------------------------------------------
+-- many to many
+------------------------------------------------------
+-- zad_1
+------------------------------------------------------
+ALTER TABLE orders ADD PRIMARY KEY(id);
+ALTER TABLE orders MODIFY COLUMN id int unsigned auto_increment;
+
+INSERT INTO orders(description) VALUES
+('Order 1'),
+('Order 2'),
+('Order 3'),
+('Order 4'),
+('Order 5'),
+('Order 6'),
+('Order 7'),
+('Order 8'),
+('Order 9'),
+('Order 10');
+
+CREATE TABLE products_orders (
+    id int unsigned auto_increment,
+    product_id int unsigned,
+    order_id int unsigned,
+    PRIMARY KEY(id),
+    FOREIGN KEY(product_id) REFERENCES products(id),
+    FOREIGN KEY(order_id) REFERENCES orders(id)
+);
+
+INSERT INTO products_orders(product_id, order_id) VALUES
+(1,1),
+(4,1),
+(8,1),
+(5,2),
+(5,3),
+(7,3),
+(2,4),
+(1,5),
+(4,5),
+(8,5),
+(1,6),
+(2,6),
+(6,6);
+
+SELECT
+    o.description,
+    p.name,
+    p.price
+FROM
+    orders o INNER JOIN products_orders po on o.id = po.order_id
+         INNER JOIN products p on po.product_id = p.id
+ORDER BY 1;
+
+------------------------------------------------------
+-- zad_2
+------------------------------------------------------
+CREATE TABLE cinemas_movies (
+                                id int unsigned auto_increment,
+                                cinema_id int not null,
+                                movie_id int not null,
+                                PRIMARY KEY(id),
+                                FOREIGN KEY(cinema_id) REFERENCES cinemas(id),
+                                FOREIGN KEY(movie_id) REFERENCES movies(id)
+);
+
+INSERT INTO cinemas_movies(cinema_id, movie_id) VALUES
+(1, 2),
+(1, 6),
+(1, 7),
+(1, 9),
+(1, 11),
+(2, 1),
+(2, 2),
+(3, 2),
+(4, 2),
+(2, 5),
+(4, 4),
+(4, 7),
+(3, 11),
+(8, 4),
+(8, 6),
+(8, 9),
+(11, 1),
+(11, 4),
+(11, 5),
+(11, 7),
+(9, 7),
+(9, 6),
+(6, 1);
+
+SELECT
+    c.name,
+    m.title
+FROM
+    cinemas c INNER JOIN cinemas_movies cm on c.id = cm.cinema_id
+              INNER JOIN movies m on cm.movie_id = m.id
+ORDER BY 1, 2;

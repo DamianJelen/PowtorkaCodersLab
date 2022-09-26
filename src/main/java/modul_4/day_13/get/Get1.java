ba94e0b3d8a5df5a78965f91983c4919164befbf
damian161@o2.pl <damian161@o2.pl>
2022-09-26 19:54:16 +0200

    day 13 get ex 1

---
+++ src/main/java/modul_4/day_13/get/Get1.java
@@ -0,0 +1,24 @@
+package modul_4.day_13.get;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet
+public class Get1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if(req.getParameter("start") != null || req.getParameter("end") != null ) {
+            int start = Integer.parseInt(req.getParameter("start"));
+            int end = Integer.parseInt(req.getParameter("end"));
+            for (int i = start; i <= end; i++) {
+                resp.getWriter().append(i + " ");
+            }
+        } else {
+            resp.getWriter().append("Brak");
+        }
+    }
+}

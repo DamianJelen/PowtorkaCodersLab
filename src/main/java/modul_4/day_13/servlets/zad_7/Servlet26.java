aa7459a06089a00bcfb1e66d473eb4093458801d
damian161@o2.pl <damian161@o2.pl>
2022-09-29 19:20:56 +0200

    day 13 servlets ex 7

---
+++ src/main/java/modul_4/day_13/servlets/zad_7/Servlet26.java
@@ -0,0 +1,32 @@
+package modul_4.day_13.servlets.zad_7;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet
+public class Servlet26 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+
+        Map<String, String> map = new HashMap<>();
+        Enumeration<String> headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = headerNames.nextElement();
+            String value = req.getHeader(key);
+            map.put(key, value);
+        }
+
+        for(Map.Entry entry : map.entrySet()) {
+            resp.getWriter().append(entry.getKey() + "").append(" : ").append(entry.getValue() + "").append("<br/>");
+        }
+    }
+}

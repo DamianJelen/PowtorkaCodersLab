605ba5ae383da631e89967f66f519fbbabf440cb
damian161@o2.pl <damian161@o2.pl>
2022-10-11 23:00:58 +0200

    day 14 sessions ex 8 to correct

---
+++ src/main/java/modul_4/day_14/sessions/zad_8/Countries.java
@@ -0,0 +1,19 @@
+package modul_4.day_14.sessions.zad_8;
+
+public class Countries {
+    private String countryName;
+    private String capitalName;
+
+    public Countries(String countryName, String capitalName) {
+        this.countryName = countryName;
+        this.capitalName = capitalName;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getCapitalName() {
+        return capitalName;
+    }
+}
---
+++ src/main/java/modul_4/day_14/sessions/zad_8/Sess38.java
@@ -0,0 +1,68 @@
+package modul_4.day_14.sessions.zad_8;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "Sess38", value = "/session38")
+public class Sess38 extends HttpServlet {
+    Countries[] countries = {new Countries("Litwa", "Wilno"),
+            new Countries("Białoruś", "Mińsk"),
+            new Countries("Ukraina", "Kijów"),
+            new Countries("Słowacja", "Bratyslawa"),
+            new Countries("Czechy", "Praga"),
+            new Countries("Niemcy", "Berlin")};
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        HttpSession session = request.getSession();
+        int licznik = 0;
+        if (session.getAttribute("licznik") == null) {
+            session.setAttribute("licznik", 0);
+        } else {
+            licznik = (int) session.getAttribute("licznik");
+            licznik++;
+            session.setAttribute("licznik", licznik);
+        }
+        response.getWriter().append("<form action=\"/session38\" method=\"post\">\n" +
+                "        <label>Write capital of " + countries[licznik].getCountryName() + " <input type=\"text\" name=\"capital\"></label>\n" +
+                "        <input type=\"submit\" value=\"next\">\n" +
+                "    </form>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        HttpSession session = request.getSession();
+        String answer = request.getParameter("capital");
+        int licznik = (int) session.getAttribute("licznik");
+        int correctAnswer = 0;
+        if (session.getAttribute("correctAnswers") == null) {
+            session.setAttribute("correctAnswers", 0);
+        } else {
+            correctAnswer = (int) session.getAttribute("correctAnswers");
+        }
+        if(answer.equals(countries[licznik].getCapitalName())) {
+            ++correctAnswer;
+            session.setAttribute("correctAnswers", correctAnswer);
+        }
+        if(licznik < countries.length - 1) {
+            response.sendRedirect("/session38");
+        }
+        if (licznik == countries.length - 1) {
+            session.removeAttribute("correctAnswer");
+            session.removeAttribute("licznik");
+            response.getWriter().append(session.getAttribute("correctAnswer") + "");
+            response.getWriter().append("Correct answers: ").append(correctAnswer + "<br/>").
+                    append("<a href=\"../session38\">Repeat test</a>");
+
+        }
+    }
+}
